7940a8e8195a6aafe43d6c97936b05756d6941a1
Marc B <mboyer5797@yahoo.com>
2018-11-14 17:23:28 -0500

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,4 +1,5 @@
 #user hits 'acquire' to start this program
+from typing import List, Any
 
 import bs4
 import re
@@ -12,18 +13,17 @@ page_html = uClient.read()
 uClient.close()
 
 page_soup = soup(page_html, "html.parser")
-print("hello")
 
-#puts all of the elements with the titles into a container
 data = []
-
 container = page_soup.find_all("a", {"class": "fontS16px"})
 
-#use regex to only pull the noteworthy and notable headlines
+#use regex to only pull the noteworthy and notable symbols from the headlines
 for a in container:
-    search = re.search("Option Activity:", a.text)
-    data.append(search)
+    symSearch = re.findall("Option Activity:(.[^<]*)", a.text)
+    if symSearch:
+	    for r in symSearch:
+                data.append(r.strip())
+                print(r)
 
+print("Printing data[]: ")
 print(data)
-
-
