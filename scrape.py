f14b7ce9a9a1cc539a80a9b6b9cae360d07c3ed8
Marc B <mboyer5797@yahoo.com>
2018-11-14 22:40:30 -0500

    Adds headline, text and improves symbol scrape
    need to improve text scrape and format the final[]

--- scrape.py
+++ scrape.py
@@ -1,5 +1,4 @@
-#user hits 'acquire' to start this program
-from typing import List, Any
+# user hits 'acquire' to start this program
 
 import bs4
 import re
@@ -7,31 +6,58 @@ from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 my_url = 'https://www.nasdaq.com/options/'
 
-#open connection to get the web page
+# open connection to get the web page
 uClient = uReq(my_url)
 page_html = uClient.read()
 uClient.close()
 
 page_soup = soup(page_html, "html.parser")
 
-symbols = []
+final = []
+headline = []
+symbol = []
+info = []
 url = []
-container = page_soup.find_all("a", {"class": "fontS16px"})
+container = page_soup.find_all('a', {"class": "fontS16px"})
 
-#use regex to only pull the SYMBOLS that we want
+# use regex to only pull the SYMBOLS that we want
 for a in container:
     symSearch = re.findall("Option Activity:(.[^<]*)", a.text)
     if symSearch:
         for r in symSearch:
-            symbols.append(r.strip())
-#pull the URLs that we want
+            symbol.append(r.strip().split(", "))
+# opens article page to scrape more data
 for a in container:
-    if re.search("Option Activity:(.[^<]*)", a.text) != None:
-        print("url found")
-        url.append(a.get('href'))
+    if re.search("Option Activity:(.[^<]*)", a.text) is not None:
+        # pulls the URL
+        article = a.get('href')
+        url.append(article)
+        # load the NEW web page of the article we want
+        uClient = uReq(article)
+        page_html = uClient.read()
+        uClient.close()
+        page_soup = soup(page_html, "html.parser")
+        # pulls the headline and put into list
+        headline.append(page_soup.find('h1').text)
+        # pulls the text from the article
+        info.append(page_soup.find('div', {"id": "articleText"}).text.strip().split('       '))
 
-print("Printing symbols[]: ")
-print(symbols)
+# combine the lists of lists caused by split() back into a normal list
+symbolsList = []
+for l in symbol:
+    symbolsList += l
+infoList = []
+for l in info:
+    infoList += l
+
+print("Printing symbolsList[]: ")
+print(symbolsList)
 
 print("Printing url[]: ")
 print(url)
+
+print("Printing headline[]: ")
+print(headline)
+
+print("Printing infoList[]: ")
+print(infoList)
